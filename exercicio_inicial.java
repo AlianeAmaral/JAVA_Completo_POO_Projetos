e3c41d1010ea6d5d9d9ea0847ac36f99a711029f
Aliane Elias Amaral <108065159+AlianeElias@users.noreply.github.com>
2022-07-18 11:37:58 -0400

    Rename exercicio_resolvido.java to exercicio_inicial.java

--- exercicio_inicial.java
+++ exercicio_inicial.java
@@ -8,19 +8,19 @@ public class exercicio_resolvido {
 	Locale.setDefault(Locale.US);
 	Scanner sc = new Scanner(System.in);
 	
-	System.out.print("Qual È a largura do terreno? R: ");	
+	System.out.print("Qual √© a largura do terreno? R: ");	
 	double largura = sc.nextDouble();
-	System.out.print("Qual È o comprimento do terreno? R: ");
+	System.out.print("Qual √© o comprimento do terreno? R: ");
 	double comprimento = sc.nextDouble();
-	System.out.print("Qual È o valor do terreno por metro quadrado? R: ");	
+	System.out.print("Qual √© o valor do terreno por metro quadrado? R: ");	
 	double metro = sc.nextDouble();
 	
 	double area = (largura * comprimento);
 	double total = (metro * area);
 	
 	System.out.println();
-	System.out.printf("O terreno possui ·rea de: %.2f metros quadrados.%n", area);
-	System.out.printf("O terreno est· custando no total de: %.2f reais.%n", total);
+	System.out.printf("O terreno possui √°rea de: %.2f metros quadrados.%n", area);
+	System.out.printf("O terreno est√° custando no total de: %.2f reais.%n", total);
 
 	sc.close();
 
