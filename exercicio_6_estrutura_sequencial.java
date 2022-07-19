ab4c84d917b80934d364f134271b17a32b602ebb
Aliane Elias Amaral <108065159+AlianeElias@users.noreply.github.com>
2022-07-19 10:12:16 -0400

    Add files via upload

---
+++ exercicio_6_estrutura_sequencial.java
@@ -0,0 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio_6_estrutura_sequencial {
+
+	public static void main(String[] args) {
+	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double A, B, C;
+		
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		C = sc.nextDouble();
+		
+		double triangulo, circulo, trapezio, quadrado, retangulo;
+		double pi = 3.14159;
+		
+		triangulo = A * C / 2;
+		circulo = (C * C) * pi;
+		trapezio = (A + B) / 2.0 * C;
+		quadrado = B * B;
+		retangulo = A * B;
+		
+		System.out.printf("A área do triângulo é: %.3f %n", triangulo);
+		System.out.printf("A área do círculo é: %.3f %n", circulo);
+		System.out.printf("A área do trapezio é: %.3f %n", trapezio);
+		System.out.printf("A área do quadrado é: %.3f %n", quadrado);
+		System.out.printf("A área do retângulo é: %.3f %n", retangulo);
+		
+		sc.close();
+
+	}
+
+}
