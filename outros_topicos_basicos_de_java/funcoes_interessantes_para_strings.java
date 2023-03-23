2328f8d9a426a09d895917e8129b31fdcf7110c2
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-03-22 21:05:30 -0400

    funcoes_interessantes_para_strings.java

---
+++ outros_topicos_basicos_de_java/funcoes_interessantes_para_strings.java
@@ -0,0 +1,32 @@
+package estudos_programacao;
+
+public class funcoes_interessantes_para_strings {
+
+	public static void main(String[] args) {
+	
+		String original = "abcde FGHIJ ABC abc DEFG ";
+		
+		String s01 = original.toLowerCase();
+		String s02 = original.toUpperCase();
+		String s03 = original.trim();
+		String s04 = original.substring(2);
+		String s05 = original.substring(2, 9);
+		String s06 = original.replace('a', 'x');
+		String s07 = original.replace("abc", "xy");
+		int i = original.indexOf("bc");
+		int j = original.lastIndexOf("bc");
+		
+		System.out.println("Original: -" + original + "-");
+		System.out.println("toLowerCase: -" + s01 + "-");
+		System.out.println("toUpperCase: -" + s02 + "-");
+		System.out.println("trim: -" + s03 + "-");
+		System.out.println("substring(2): -" + s04 + "-");
+		System.out.println("substring(2, 9): -" + s05 + "-");
+		System.out.println("replace('a', 'x'): -" + s06 + "-");
+		System.out.println("replace('abc', 'xy'): -" + s07 + "-");
+		System.out.println("Index of 'bc': " + i);
+		System.out.println("Last index of 'bc': " + j);
+
+	}
+
+}
