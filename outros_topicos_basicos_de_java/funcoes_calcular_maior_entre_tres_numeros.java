4fc925131bacb6603806d9501231826bcc7aacca
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-03-23 12:00:48 -0400

    funcoes_calcular_maior_entre_tres_numeros.java

---
+++ outros_topicos_basicos_de_java/funcoes_calcular_maior_entre_tres_numeros.java
@@ -0,0 +1,28 @@
+package estudos_programacao;
+import java.util.Scanner;
+
+public class funcoes_calcular_maior_entre_tres_numeros {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter three numbers:");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		if (a > b && a > c) {
+			System.out.println("Higher = " + a);
+		} else if (b > c) {
+			System.out.println("Higher = " + b);
+		} else {
+			System.out.println("Higher = " + c);
+		
+		}
+		
+		sc.close();
+		
+	}
+}
+
