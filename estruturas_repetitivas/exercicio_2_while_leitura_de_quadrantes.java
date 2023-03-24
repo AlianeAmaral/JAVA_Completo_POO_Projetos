bedd1fb1dea165489221fba0436c1cad5504a609
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-03-24 11:34:37 -0400

    Add files via upload

---
+++ estruturas_repetitivas/exercicio_2_while_leitura_de_quadrantes.java
@@ -0,0 +1,48 @@
+package estudos_programacao;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class exercicio_2_while_leitura_de_quadrantes {
+
+	public static void main(String[] args) {
+	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+
+		System.out.println("Digite o valor de X no plano cartesiano: ");
+		double x = sc.nextDouble();
+	
+		System.out.println("Digite o valor de Y no plano cartesiano: ");
+		double y = sc.nextDouble();
+	
+		while (x != 0 && y != 0) {
+		
+			if (x > 0 && y > 0) {
+				System.out.println("O número está no quadrante Q1.");
+			}	
+		
+			else if (x < 0 && y > 0) {
+				System.out.println("O número está no quadrante Q2.");	
+			}
+		
+			else if (x < 0 && y < 0) {
+				System.out.println("O número está no quadrante Q3.");
+			}
+		
+			else {
+				System.out.println("O número está no quadrante Q4.");
+			}	
+		
+			System.out.println("Digite o valor de X no plano cartesiano: ");
+			x = sc.nextDouble();
+			
+			System.out.println("Digite o valor de Y no plano cartesiano: ");
+			y = sc.nextDouble();
+		
+		}
+	
+		sc.close();
+	
+	}
+
+}
