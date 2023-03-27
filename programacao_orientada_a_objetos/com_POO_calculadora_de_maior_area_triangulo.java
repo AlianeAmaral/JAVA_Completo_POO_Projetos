fe11ffee1b17f3f383575784fb43efbaa232491b
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-03-27 06:46:16 -0400

    Create com_POO_calculadora_de_maior_area_triangulo.java

---
+++ programacao_orientada_a_objetos/com_POO_calculadora_de_maior_area_triangulo.java
@@ -0,0 +1,31 @@
+package application;
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Triangle;
+
+  public class Program {
+    
+  public static void main(String[] args) {
+
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+    
+    Triangle x, y;
+    x = new Triangle();
+    y = new Triangle();
+    
+    System.out.println("Enter the measures of triangle X: ");
+    x.a = sc.nextDouble();
+    x.b = sc.nextDouble();
+    x.c = sc.nextDouble();
+    
+    System.out.println("Enter the measures of triangle Y: ");
+    y.a = sc.nextDouble();
+    y.b = sc.nextDouble();
+    y.c = sc.nextDouble();
+
+    double p = (x.a + x.b + x.c) / 2.0;
+    double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+
+    p = (y.a + y.b + y.c) / 2.0;
+    double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
