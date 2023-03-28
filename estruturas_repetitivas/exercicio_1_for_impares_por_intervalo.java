bb68fff3ff0d106d072ab8f132a8f8c7195e8da6
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-03-27 21:53:00 -0400

    Add files via upload

---
+++ estruturas_repetitivas/exercicio_1_for_impares_por_intervalo.java
@@ -0,0 +1,26 @@
+package estudos_programacao;
+import java.util.Scanner;
+
+public class exercicio_1_for_impares_por_intervalo {
+
+	public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+
+	System.out.println("Digite o valor inteiro de X: ");
+	int X = sc.nextInt();
+	
+	for (int i=0; i <= X; i++) {
+		// o i <= 0 determina o limite da operação, a condição para interromper.
+		
+		if (i % 2 != 0) {
+			System.out.println(i);
+			//o resto que for diferente de 0, vai entrar como "i", ou seja, os ímpares.
+		}
+	
+	}
+	
+	sc.close();
+	}
+
+}
