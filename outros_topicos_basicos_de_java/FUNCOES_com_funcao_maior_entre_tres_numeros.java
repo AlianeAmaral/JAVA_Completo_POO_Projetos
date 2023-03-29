68cd7048ec2f96ad931db329b8b39764474d7ad2
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-03-29 07:02:30 -0400

    FUNCOES_com_funcao_maior_entre_tres_numeros.java

---
+++ outros_topicos_basicos_de_java/FUNCOES_com_funcao_maior_entre_tres_numeros.java
@@ -0,0 +1,37 @@
+package estudos_programacao;
+import java.util.Scanner;
+  
+public class FUNCOES_com_funcao_maior_entre_tres_numeros {
+  
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter three numbers:");
+  
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+  
+        int higher = max(a, b, c);
+        showResult(higher);
+
+        sc.close();
+    }
+    
+    public static int max(int x, int y, int z) {
+        int aux;
+        if (x > y && x > z) {
+            aux = x;
+        } else if (y > z) {
+            aux = y;
+        } else {
+            aux = z;
+        }
+    return aux;
+    }
+    
+    public static void showResult(int value) {
+        System.out.println("Higher = " + value);
+    }
+}
