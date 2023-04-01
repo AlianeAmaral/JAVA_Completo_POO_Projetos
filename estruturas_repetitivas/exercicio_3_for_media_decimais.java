4aaf53e6177b7e32f358f0398e4626b91eecea1f
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-04-01 15:52:58 -0400

    Add files via upload

---
+++ estruturas_repetitivas/exercicio_3_for_media_decimais.java
@@ -0,0 +1,34 @@
+package estudos_programacao;
+import java.util.Scanner;
+
+public class exercicio_3_for_media_decimais {
+
+	public static void main(String[] args) {
+	
+	Scanner sc = new Scanner (System.in);
+	
+	int repeticoes = 0;
+	double x, y, z;
+	
+	System.out.println("Quantas vezes deseja repetir? ");
+	repeticoes = sc.nextInt();
+	
+	for (int i=0; i < repeticoes; i++) {
+		System.out.println("Digite o primeiro número decimal: ");
+			x = sc.nextDouble();
+	
+		System.out.println("Digite o segundo número decimal: ");
+			y = sc.nextDouble();
+	
+		System.out.println("Digite o terceiro número decimal: ");
+			z = sc.nextDouble();
+		
+		double mediaDecimais = ((x * 2) + (y * 3) + (z * 5)) / 10;
+		System.out.printf("A média dos decimais informados é: %.1f%n", mediaDecimais);
+	}
+	
+	sc.close();
+	
+	}
+
+}
