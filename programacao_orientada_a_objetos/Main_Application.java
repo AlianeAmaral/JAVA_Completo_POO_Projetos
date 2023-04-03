e69e12a0febb35669fd459ecb8c8fdb0a2740b96
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-04-03 07:07:56 -0400

    Add files via upload

---
+++ programacao_orientada_a_objetos/Main_Application.java
@@ -0,0 +1,61 @@
+package controle_de_estoque;
+import java.util.Scanner;
+
+public class Main_Application {
+
+	public static void main(String[] args) {
+		
+	    Scanner sc = new Scanner(System.in);
+	    
+	    System.out.println("Deseja trabalhar com um novo produto (s/n)? ");
+	    char resposta = sc.next().charAt(0);
+	    
+		while (resposta == 's') {
+	    
+	    System.out.println("Deseja adicionar (a) ou remover (r) uma quantidade? Para encerrar, inserir qualquer outro dígito.");
+	    char repetir = sc.next().charAt(0);
+	    
+	    	if (repetir == 'a') {
+	    	    Product produto;
+	    	    produto = new Product();
+	    	    
+	    	    System.out.println("Entre com as informações do produto (nome/preço/quantidade): ");
+	    	    produto.name = sc.next();
+	    	    produto.price = sc.nextDouble();
+	    	    produto.quantity = sc.nextDouble();
+	    	    
+	    	    System.out.println("Nome do Produto: " +produto.name+ " / Preço: $ "+produto.price+ " / Quantidade: " +produto.quantity+ " unidades. Valor Total no Estoque: " +produto.totalPrice());    
+	    	
+	    	    System.out.println("Quantidade que deseja adicionar: ");
+	    	    produto.adicionar = sc.nextDouble();
+	    	    
+	    	    System.out.println("Nome do Produto: " +produto.name+ " / Preço: R$ "+produto.price+ " / Quantidade: " +produto.quantityAdd()+ " unidades. Valor Total no Estoque: R$ " +produto.totalPriceAdd());    
+		    	
+	    	}
+	    	else if (repetir == 'r') {
+	    	    Product produto;
+	    	    produto = new Product();
+	    	    
+	    	    System.out.println("Entre com as informações do produto (nome/preço/quantidade): ");
+	    	    produto.name = sc.next();
+	    	    produto.price = sc.nextDouble();
+	    	    produto.quantity = sc.nextDouble();
+	    	    
+	    	    System.out.println("Nome do Produto: " +produto.name+ " / Preço: $ "+produto.price+ " / Quantidade: " +produto.quantity+ " unidades. Valor Total no Estoque: " +produto.totalPrice());    
+	    	
+	    	    System.out.println("Quantidade que deseja remover: ");
+	    	    produto.remover = sc.nextDouble();
+	    	    
+	    	    System.out.println("Nome do Produto: " +produto.name+ " / Preço: R$ "+produto.price+ " / Quantidade: " +produto.quantityRemoved()+ " unidades. Valor Total no Estoque: R$ " +produto.totalPriceRemoved());    	
+	    	}
+	    
+		System.out.println("Deseja trabalhar com um novo produto (s/n)? ");
+		resposta = sc.next().charAt(0);
+    	
+		}
+    	
+		System.out.println("Aplicação Encerrada.");
+		
+	    sc.close();
+	}
+}
---
+++ programacao_orientada_a_objetos/Product.java
@@ -0,0 +1,38 @@
+package controle_de_estoque;
+
+public class Product {
+
+	public String name;
+	public double price;
+	public double quantity;
+	
+	public double adicionar;
+	public double remover;
+	
+	public double totalPrice() {
+		double calculo1 = quantity * price;
+	return calculo1;	
+	}
+	
+	public double totalPriceAdd() {
+		double calculo2 = ((adicionar + quantity) * price);
+	return calculo2;
+	}
+	
+	public double quantityAdd() {
+		double calculo3 = adicionar + quantity;
+	return calculo3;
+	}
+	
+	public double totalPriceRemoved() {
+		double calculo4 = ((quantity - remover) * price);
+	return calculo4;
+	}
+	
+	public double quantityRemoved() {
+		double calculo5 = quantity - remover; 
+	return calculo5;
+	}
+	
+}
+	
